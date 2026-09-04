21214c7646eb17f1be465b8db2994578a1905fb9
Sathwik Reddy <sathwikreddyatmakurusr@gmail.com>
2026-09-04 09:23:28 +0530

    Update Case_Study-04.sql

--- Case_Study-04.sql
+++ Case_Study-04.sql
@@ -3,10 +3,10 @@
 --Date:28-06-2025
 
 --Q1: Write a query to return player_name, school_name, position, conference from the above dataset ?
-select a.player_name,a.school_name,a.position,b.conference                                     --selecting multiple columns from two tables
+select a.player_name,a.school_name,a.position,b.conference                                      --selecting multiple columns from two tables
 from benn.college_football_players as a                                                         --giving alias name,table 1
 join benn.college_football_teams as b                                                          -- joining the table and giving alias name,table 2
-on a.school_name=b.school_name;                                                                --giving similar columns in both tables to perform join
+on a.school_name=b.school_name;                                                                 --giving similar columns in both tables to perform join
 
 --Q2: Write a query to find the total number of players playing in each conference.Order the output in the descending order of number of players ?
 select b.conference,count(*) as total_players                                                    --selecting a column from table 2 and counting all records and giving alias name
